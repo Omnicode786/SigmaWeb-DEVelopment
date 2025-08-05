345d41a376188e854f929aff8d7ec7506f50b9a3
Muzammil Alam <muzammilalam408@gmail.com>
2025-08-06 03:09:59 +0500

    commit

--- JavaScript/video67/Async/fetch.js
+++ JavaScript/video67/Async/fetch.js
@@ -3,9 +3,16 @@ let  x = await fetch("https://jsonplaceholder.typicode.com/todos/1");
 // ruka ruka fetch kia data jesei hogya neche wala code run hgya
 
 
+// fetch api aik promise return krta he then ham usko parse krte hen or wo bhi aik promise return krta he
+
 let data = await x.json();
 // ab ham iske json hone ka wait kr rhe hen
 console.log(data);
+console.log(data.id);
+// as thsi was not an array of objects
+console.log(data.title);
 
 
 }
+
+API();
