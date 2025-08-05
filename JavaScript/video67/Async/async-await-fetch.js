7cc1ae39a98c31c28b61459d9c79a45418742a0e
Muzammil Alam <164224694+Omnicode786@users.noreply.github.com>
2025-08-05 17:40:23 +0500

    comit

---
+++ JavaScript/video67/Async/async-await-fetch.js
@@ -0,0 +1,38 @@
+
+
+function getData(){
+return new Promise((resolve,reject) => {
+    setTimeout (() => {
+        resolve(455);
+    }, 5000)
+})
+
+
+}
+
+
+
+console.log("loading data");
+console.log("loading charactr data");
+console.log("loading mean dta");
+
+let data = getData();
+
+console.log(data);
+// data ayega hi ni 
+// jab tak promise resolve ni hoti 
+// neche ka sara code chal jayega because java script is async \
+//  to stop this behavou we use async
+
+// there is anohter method
+data.then((a) => {
+    console.log(a);
+    console.log("Data has been recieved")
+})
+// if we dont watch this approach
+
+
+// lets just make a asybc
+
+
+console.log("process data");
