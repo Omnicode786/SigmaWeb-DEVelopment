6b1e62d9e622b3f81ebd3fada2a62ba1424e38b8
Muzammil Alam <muzammilalam408@gmail.com>
2025-08-06 02:59:59 +0500

    Auto commit

--- JavaScript/video67/Async/fetch.js
+++ JavaScript/video67/Async/fetch.js
@@ -1,6 +1,7 @@
 async function API() {
 let  x = await fetch("https://jsonplaceholder.typicode.com/todos/1");
-// ruka ruka fet
+// ruka ruka fetch kia data jesei hogya neche wala code run hgya
+
 
 let data = await x.json();
 
