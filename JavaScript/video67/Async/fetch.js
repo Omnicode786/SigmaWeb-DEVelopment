16480bcaa4b034de6b81d0fe83da0e466e9522d5
Muzammil Alam <muzammilalam408@gmail.com>
2025-08-06 21:29:52 +0500

    up

--- JavaScript/video67/Async/fetch.js
+++ JavaScript/video67/Async/fetch.js
@@ -7,6 +7,7 @@ let  x = await fetch("https://jsonplaceholder.typicode.com/todos/1");
 
 let data = await x.json();
 // ab ham iske json hone ka wait kr rhe hen
+// json data ko objectify kr deta he
 console.log(data);
 console.log(data.id);
 // as thsi was not an array of objects
@@ -16,3 +17,7 @@ console.log(data.title);
 }
 
 API();
+
+// promise ka settle hone ka mtlb he eitehr wo resolve hojaye or reject hojaye
+// reolve means settled succesfully
+// reject measn not settled succesffuly
