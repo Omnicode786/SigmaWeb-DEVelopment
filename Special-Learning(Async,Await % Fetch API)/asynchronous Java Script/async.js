e317346805556bbc600b205c90a298e437ff0128
Muzammil Alam <164224694+Omnicode786@users.noreply.github.com>
2025-08-08 12:41:47 +0500

    c

--- Special-Learning(Async,Await % Fetch API)/asynchronous Java Script/async.js
+++ Special-Learning(Async,Await % Fetch API)/asynchronous Java Script/async.js
@@ -2,3 +2,17 @@
 
 // aisa code jo jab chalne ke lie ready ho jate tab chale wo hai async
 
+function FetchTheProfile(username, callback){
+    setTimeout(() => {
+        console.log(`Profile fetched of ${username}`);
+        callback({username,
+            role: "Developer",
+            rank: "Power IV"
+        }) // this simpl y means username: username})
+    }, 2000);
+}
+
+
+FetchTheProfile("Muzamml", function(profileData){
+    console.log("The profile data fetched came out as: ", profileData)
+})
