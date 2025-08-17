50a0115865b2f38f1f583776198ea86623e5c751
Muzammil Alam <muzammilalam408@gmail.com>
2025-08-18 02:54:24 +0500

    commit

---
+++ NodeJS/sherians/PART4-FORMS-SESSIONS-COOKIES/cookie.js
@@ -0,0 +1,9 @@
+// session id and cookeis remember
+
+// kch data ham frontend pr store kr wskte hen phir jab bhi backend pr request jayegi wo stored info bhi sath chlejayega backend me 
+// example login ingo
+
+// login se logout tak ka session 
+
+// jab ham user se data lekr bhejte hen to wo data hamen in a blob format as in stream me milta he which is not 
+// directly readble
