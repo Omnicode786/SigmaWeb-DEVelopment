29bb2d41e544e5ef81ca53ad0ceb27d4b0ba371d
Muzammil Alam <muzammilalam408@gmail.com>
2025-09-07 04:13:21 +0500

    c

--- ReactJS/PART3-tailwindand_props/src/App.jsx
+++ ReactJS/PART3-tailwindand_props/src/App.jsx
@@ -1,16 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
+"use client";
+
+
+import Card1 from './components/Card1';
+
+import CArd2 from './components/CArd2';
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1 className='bg'>Hello World</h1>
+      <h1 className='bg-amber-400 text-black'>Hello World</h1>
+      <h3>Prosp makes components reusable</h3>
+    <Card1 username = "Muzammil Alam"/>
+ <CArd2 username = "Muzammil Alam"/>
+
+   
     </>
   )
 }
+
+
+
 
 export default App
