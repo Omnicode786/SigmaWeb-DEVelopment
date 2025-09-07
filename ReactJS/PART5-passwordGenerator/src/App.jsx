955578d6be09cf3a1115606dcd29b58076153885
Muzammil Alam <164224694+Omnicode786@users.noreply.github.com>
2025-09-07 10:05:34 -0700

    c

--- ReactJS/PART5-passwordGenerator/src/App.jsx
+++ ReactJS/PART5-passwordGenerator/src/App.jsx
@@ -36,13 +36,60 @@ setPassword(pass);
   return (
     <>
 
-      <div className='w-full max-w-md mx-auto shadow-md px-4 my-8 rounded-lg text-orange-600 bg-gray-500 text-center'>test</div>
-      <div className='w-full max-w-md mx-auto shadow-md px-4 my-8 rounded-lg text-orange-600 bg-gray-500 text-center'>
+      <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800 text-orange-500">
+
+        <h1 className='text-white text-center m-2 '>Password Generator</h1>
+   
+      <div className='flex shadow rounded-lg overflow-hidden mb-4 bg-white'>
+
+      
       <input type="text"
       value = {Password} 
-      className='outline-none w-full py-1 px-3' />
+      className='outline-none w-full py-1 px-3'
+      placeholder='password'
+      readOnly  
+      
+      />
+    <button className='outline-none  py-0.5 shrink-0 px-3 bg-blue-700 text-white'>Copy</button>
 
+      </div>
+      <div className='flex text-sm gap-x-2'>
+        <div className='flex items-center gap-x-1'>
+          <input 
+          type="range" 
+          min = {8}
+          max = {100}
+          value={length}
+          className='cursor-pointer'
+          onChange={(e)=> {setLength(e.target.value)}}
+          />
+          <label>Length: {length}</label>
+        </div>
+         <div className='flex items-center gap-x-1'>
+          <input 
+          type="checkbox" 
+          className='cursor-pointer'
+          onChange={() => {setNumberAllowed((prev) => !prev)
+          console.log(charAllowed);
 
+          }}
+          />
+          <label>Numbers</label>
+        </div>
+          <div className='flex items-center gap-x-1'>
+          <input 
+          type="checkbox" 
+          className='cursor-pointer'
+          onChange={() => {setCharAllowed((prev) => !prev) 
+          console.log(charAllowed);
+          }
+          
+        }
+          />
+          
+          <label>Characters</label>
+        </div>
+      </div>
 
 
       </div>
