24fdc1717e239fb6bdc0d6f49921fe25f3aa163e
Muzammil Alam <164224694+Omnicode786@users.noreply.github.com>
2025-09-07 06:21:29 -0700

    j

--- ReactJS/PART4-bgChanger/src/App.jsx
+++ ReactJS/PART4-bgChanger/src/App.jsx
@@ -12,7 +12,19 @@ function App() {
 
 
     
-    <div className='w-full h-screen duration-200' style={{backgroundColor:color}}> HELLo </div>
+    <div className='w-full h-screen duration-200' style={{backgroundColor:color}}> HELLo 
+    
+    <div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2'>Test
+<div className='flex flex-wrap gap-3 justify-center shadow-lg bg-white rounded-lg'>
+
+  <button>Red</button>
+</div>
+
+    </div>
+    
+    
+    
+    </div>
 
 
   
