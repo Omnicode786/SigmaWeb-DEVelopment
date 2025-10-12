49c17cc1488b6ae0894d731f460ed7ba258dd99e
Muzammil Alam <164224694+Omnicode786@users.noreply.github.com>
2025-10-12 22:09:50 +0500

    c

---
+++ ReactJsSheriansUltimate/Chapter2/Styling-modules/src/components/Hero/Hero.jsx
@@ -0,0 +1,20 @@
+import styles from './Hero.module.css'
+
+
+
+
+function Hero(){
+    return (
+        <>
+        <div  className={styles.Hero}>
+        <h1>The Beast Be Cooking</h1>
+            <p className={styles.para}>Hey there! Im an imaginative artist and storyteller who loves turning simple sketches into worlds full of character and emotion. My art blends cozy vibes, expressive design, and a touch of adventure</p>
+        
+        </div>
+    
+        </>
+    )
+
+}
+
+export default Hero
